f988a23fece6cf11bc6577c475d61bea653d27a2
bstanfield1992 <bstanfield1992@gmail.com>
2021-01-24 14:07:33 -0700

    added basic fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
 // add variables
-
-
+var searchedName = document.getElementById('searchedName')
+var 
 // add fetch
+searchedName = 'tucson'
 
+fetch('https://api.openweathermap.org/data/2.5/weather?q='
+ + searchedName +
+'&appid=0dedb61addcebc5a71ee712c4f54186d')
+  .then(response => response.json())
+  .then(data => console.log(data));
 
 // on load search
+
 
 
 // create cards for future days
