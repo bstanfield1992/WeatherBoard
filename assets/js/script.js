1576a44b65cfe6f49d9df1695258f393eb423aaf
bstanfield1992 <bstanfield1992@gmail.com>
2021-01-24 19:09:45 -0700

    changed how the js file functions added UV API and five day API

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,42 +3,74 @@ var searchedName = document.querySelector('#searchName').value;
 var todaysDateText = document.getElementById('todaysDate');
 var todaysDate = moment();
 
+function searchFunction() {
+var todaysUV = document.getElementById('todaysUV');
+var futureCards = document.getElementById('futureCards');
+var city = document.getElementById('city');
 
-todaysDate.format('MM DD YYYY')
-// add fetch
-function getData(searchedName) {
 
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='
-    + searchedName +
-   '&appid=0dedb61addcebc5a71ee712c4f54186d')
-     .then(response => response.json())
-     .then(data => console.log(data));
-    futureWeather(data);
+
+
+// fill out todays weather
+
+city.textContent = searchedName;
+todaysDateText.textContent = todaysDate.format('MM/DD/YYYY') + ')';
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q='
++ searchedName + '&appid=7501ebd3228baf3b901ed2b41be06662')
+ .then(response => response.json())
+ .then(data => console.log(data));
+
+// getUV();
+
+    
+// create cards for future days
+
+generateCards();
+
+
+
+
+
+
+
+  
+    
+
+    
 }
 
-
 // on load search
-    getData(searchedName)
-// create cards for future days
-function futureWeather(data) {
+
+searchFunction()
+
+function generateCards() {
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q='
+    + searchedName + '&appid=7501ebd3228baf3b901ed2b41be06662')
+     .then(response => response.json())
+     .then(data => console.log(data));
+
     for(var i = 1; i < 6; i++) {
         var dayVar = 'date' + [i]
-        var icon = date.
+        // var icon = date.;
         dayVar.toString();
         var day = document.getElementById(dayVar);
-        day.textContent = todaysDate.add(1, 'd').format('MM DD YYYY').toString();
+        day.textContent = todaysDate.add(1, 'd').format('MM/DD/YYYY').toString();
         
     }
 }
+//get UV index
 
-futureWeather();
-// add search button functionality
-function searchFunction() {
-    debugger;
-    
-    console.log(searchedName);
-    getData(searchedName);
-    
-};
+function getUV() {
+    fetch('https://http://api.openweathermap.org/data/2.5/uvi?lat='
+    + latitude + '&lon=' + longitude + '&appid=7501ebd3228baf3b901ed2b41be06662')
+ .then(response => response.json())
+ .then(data => console.log(data));
+
+    // var uv = ;
+    // return uv;
+}
+
+
 
 // add past searches
