be16233ad4fe671601917d5511a80cdb5013d8ae
bstanfield1992 <bstanfield1992@gmail.com>
2021-01-24 19:44:33 -0700

    adding uv index

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,10 @@
 var searchedName = document.querySelector('#searchName').value;
 var todaysDateText = document.getElementById('todaysDate');
 var todaysDate = moment();
+var todaysUV = document.getElementById('todaysUV');
 
 function searchFunction() {
-var todaysUV = document.getElementById('todaysUV');
+
 var futureCards = document.getElementById('futureCards');
 var city = document.getElementById('city');
 
@@ -18,24 +19,24 @@ todaysDateText.textContent = todaysDate.format('MM/DD/YYYY') + ')';
 
 fetch('https://api.openweathermap.org/data/2.5/weather?q='
 + searchedName + '&appid=7501ebd3228baf3b901ed2b41be06662')
- .then(response => response.json())
- .then(data => console.log(data));
+.then(function(response) {
+    return response.json();
+  })
+.then(function(response) {
+    console.log(response);
 
-// getUV();
-
+ var latitude = response.coord.lat;
+ console.log(latitude);
+ var longitude = response.coord.lon;
+ console.log(longitude);
+getUV(latitude, longitude);
+});
     
 // create cards for future days
 
 generateCards();
 
 
-
-
-
-
-
-  
-    
 
     
 }
@@ -47,21 +48,25 @@ searchFunction()
 function generateCards() {
     fetch('https://api.openweathermap.org/data/2.5/forecast?q='
     + searchedName + '&appid=7501ebd3228baf3b901ed2b41be06662')
-     .then(response => response.json())
-     .then(data => console.log(data));
+    .then(function(response) {
+        return response.json();
+      })
+    .then(function(response) {
+        console.log(response);
 
-    for(var i = 1; i < 6; i++) {
-        var dayVar = 'date' + [i]
-        // var icon = date.;
-        dayVar.toString();
-        var day = document.getElementById(dayVar);
-        day.textContent = todaysDate.add(1, 'd').format('MM/DD/YYYY').toString();
-        
-    }
-}
+        for(var i = 1; i < 6; i++) {
+            var dayVar = 'date' + [i]
+            // var icon = date.;
+            dayVar.toString();
+            var day = document.getElementById(dayVar);
+            day.textContent = todaysDate.add(1, 'd').format('MM/DD/YYYY').toString();
+            
+        }
+    });   
+};
 //get UV index
 
-function getUV() {
+function getUV(latitude, longitude) {
     fetch('https://http://api.openweathermap.org/data/2.5/uvi?lat='
     + latitude + '&lon=' + longitude + '&appid=7501ebd3228baf3b901ed2b41be06662')
  .then(response => response.json())
@@ -69,7 +74,7 @@ function getUV() {
 
     // var uv = ;
     // return uv;
-}
+};
 
 
 
